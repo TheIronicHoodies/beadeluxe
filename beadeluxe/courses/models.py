2a5d0aaaea1493ef6ba59e526cea535d7d2b0c2c
Katoteshiku <andre11obrero@gmail.com>
2026-02-25 13:30:05 +0800

    Course Listing Works
    - courses are currently universal
    - links to the individual courses are kinda fuckass
    - courses are added through the shell

--- beadeluxe/courses/models.py
+++ beadeluxe/courses/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Course(models.Model):
+    code = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+
+    # To be implemented
+    # beadle = models.CharField()
+    # professor = models.CharField()
+    # students = models.CharField()
+
+    def __str__(self):
+        return '{}: {}'.format(self.code, self.name)
+
+    def get_absolute_url(self):
+        return reverse('course_detail', args=[str(self.id)])
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'course'
+        verbose_name_plural = 'courses'
--- beadeluxe/courses/views.py
+++ beadeluxe/courses/views.py
@@ -1,4 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Course
 
 # Create your views here.
+# # Function-based version
+def course_list(request):
+    courses = Course.objects.all()
+    ctx = {
+        "courses": courses
+    }
+
+    return render(request, 'courses/course_list.html', ctx)
+
+def course_detail(request, id):
+    course = Course.objects.get(id=id)
+    ctx = {'course' : Course.objects.get(id=id)}
+    # ctx = {'course' : course}
+    return render(request, 'courses/course_detail.html', ctx)
+
+
+# Class-based version
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+# class CourseListView(ListView):
+#     model = Course
+#     template_name = 'courses/course_list.html'
+
+# class CourseDetailView(DetailView):
+#     model = Course
+#     template_name = 'courses/course_detail.html'
