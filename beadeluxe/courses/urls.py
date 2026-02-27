d7643221edbd40501d753155911f1de8271f4622
Katoteshiku <andre11obrero@gmail.com>
2026-02-27 10:01:25 +0800

    Changed from FBV to CBV and styled Course Listing

--- beadeluxe/courses/urls.py
+++ beadeluxe/courses/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import course_list, course_detail
-# from .views import CourseListView, CourseDetailView
+# from .views import course_list, course_detail
+from .views import CourseListView, CourseDetailView
 
 urlpatterns = [
-    path('', course_list, name="course_list"),
-    path('<int:id>', course_detail, name="course_detail"),
-    # path('list', CourseListView.as_view(), name='list'),
-    # path('<int:id>/detail', CourseDetailView.as_view(), name='course-detail'),
+    # path('', course_list, name="course_list"),
+    # path('<int:id>', course_detail, name="course_detail"),
+    path('', CourseListView.as_view(), name='list'),
+    path('<int:pk>/', CourseDetailView.as_view(), name='course-detail'),
 ]
 
 app_name = "courses"
--- beadeluxe/courses/views.py
+++ beadeluxe/courses/views.py
@@ -2,30 +2,38 @@ from django.shortcuts import render
 from .models import Course
 
 # Create your views here.
-# # Function-based version
-def course_list(request):
-    courses = Course.objects.all()
-    ctx = {
-        "courses": courses
-    }
+# Function-based version
+# def course_list(request):
+#     courses = Course.objects.all()
+#     ctx = {
+#         "courses": courses
+#     }
 
-    return render(request, 'courses/course_list.html', ctx)
+#     return render(request, 'courses/course_list.html', ctx)
 
-def course_detail(request, id):
-    course = Course.objects.get(id=id)
-    ctx = {'course' : Course.objects.get(id=id)}
-    # ctx = {'course' : course}
-    return render(request, 'courses/course_detail.html', ctx)
+# def course_detail(request, id):
+#     course = Course.objects.get(id=id)
+#     ctx = {'course' : Course.objects.get(id=id)}
+#     # ctx = {'course' : course}
+#     return render(request, 'courses/course_detail.html', ctx)
 
 
 # Class-based version
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-# class CourseListView(ListView):
-#     model = Course
-#     template_name = 'courses/course_list.html'
+class CourseListView(ListView):
+    model = Course
+    template_name = 'courses/course_list.html'
 
-# class CourseDetailView(DetailView):
-#     model = Course
-#     template_name = 'courses/course_detail.html'
+    def post(self, request, *args, **kwargs):
+        c = Course()
+        c.name = request.POST.get('course_name')
+        c.code = request.POST.get('course_code')
+        c.save()
+
+        return self.get(request, *args, **kwargs)
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course_detail.html'
