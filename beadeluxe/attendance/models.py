5cbd17b9405910df7d361177ae7b37976bc27602
NoahJVV <152837418+NoahJVV@users.noreply.github.com>
2026-03-12 09:44:36 +0800

    Created a base attendance model
    Forgot to save before properly pushing

--- beadeluxe/attendance/models.py
+++ beadeluxe/attendance/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Attendance(models.Model):
+    student_name = models.CharField(max_length=100)
+    date = models.DateField()
+    status = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.student_name} - {self.date}"
