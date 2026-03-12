134db2d938e98c6a4d731178c249422966c2a2ec
Katoteshiku <andre11obrero@gmail.com>
2026-03-13 07:10:36 +0800

    Tests for Courses List HTML Added

--- beadeluxe/courses/tests.py
+++ beadeluxe/courses/tests.py
@@ -50,27 +50,79 @@ class TestViews(TestCase):
 
 class TestCoursesListPage(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(
+        User.objects.create_user(
             username="Wilson Depot",
             password="password"
         )
+        self.client = Client()
+
+        course_One = Course()
+        course_One.code = "WilDe 11"
+        course_One.name = "Introduction to Wilson Depot"
+        course_One.save()
+
+        course_user_One = CourseUser()
+        course_user_One.course = Course.objects.get(code="WilDe 11")
+        course_user_One.user = User.objects.get(username="Wilson Depot")
+        course_user_One.role = "student"
+        course_user_One.save()
+
+        course_Two = Course()
+        course_Two.code = "WilDe 12"
+        course_Two.name = "Introduction to Wilson Depot 2"
+        course_Two.save()
+
+        course_user_Two = CourseUser()
+        course_user_Two.course = Course.objects.get(code="WilDe 12")
+        course_user_Two.user = User.objects.get(username="Wilson Depot")
+        course_user_Two.role = "student"
+        course_user_Two.save()
+        
+        User.objects.create_user(
+            username="Wilson's Son",
+            password="password"
+        )
+
         return super().setUp()
-
-    def test_page_response_if_logged_in(self):
-        client = Client()
-        client.login(
-            username="Wilson Depot",
-            password="password"
-        )
-        url = reverse('courses:list')
-        response = client.get(url)
-
+    
     def test_page_response_if_not_logged_in(self):
-        client = Client()
         url = reverse('courses:list')
-        response = client.get(url)
+        response = self.client.get(url)
         self.assertRedirects(response, '/accounts/login/?next=/courses/', status_code=302, target_status_code=200)
 
-    def test(self):
-        # self.assertInHTML()
-        pass
+    def test_page_response_if_logged_in(self):
+        self.client.login(
+            username="Wilson Depot",
+            password="password"
+        )
+        url = reverse('courses:list')
+        response = self.client.get(url)
+        self.assertEqual(response.request['PATH_INFO'], '/courses/')
+
+    def test_if_course_is_displayed(self):
+        self.client.login(
+            username="Wilson Depot",
+            password="password"
+        )
+        url = reverse('courses:list')
+        response = self.client.get(url)
+        self.assertInHTML('<a href="/courses/1/">WilDe 11: Introduction to Wilson Depot</a>', response.text)
+    
+    def test_if_multiple_courses_displayed(self):
+        self.client.login(
+            username="Wilson Depot",
+            password="password"
+        )
+        url = reverse('courses:list')
+        response = self.client.get(url)
+        self.assertInHTML('<a href="/courses/2/">WilDe 12: Introduction to Wilson Depot 2</a>', response.text)
+    
+    def test_if_courses_not_member_of_displayed(self):
+        self.client.login(
+            username="Wilson's Son",
+            password="password"
+        )
+        url = reverse('courses:list')
+        response = self.client.get(url)
+
+        self.assertNotInHTML('<a href="/courses/1/">WilDe 11: Introduction to Wilson Depot</a>', response.text)
