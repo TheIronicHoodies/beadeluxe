8453c54b1b3b29a4ea2bfab502eab4420ec9dbbf
hanashimi <crmacaspac@gmail.com>
2026-04-15 14:50:16 +0800

    Added Test Cases Base in tests.py

--- beadeluxe/calendarApp/tests.py
+++ beadeluxe/calendarApp/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
+# View Course Calendar (Beadle, Student)
+# View Access Restriction (Beadle, Student) (Professor cannot)
+# Add Major Dates (Beadle)
+# Remove Dates (Beadle)
+# Assign Category (Beadle)
+# Edit Dates (Beadle)
+
+
+
 # Create your tests here.
