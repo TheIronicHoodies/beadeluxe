6f010938a6488dde23e43430c065128ea6837f12
Levi Si <anonymous.perrson09@gmail.com>
2026-04-17 13:44:17 +0800

    Restored changes from branch 'ux'

--- beadeluxe/chat/urls.py
+++ beadeluxe/chat/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from chat.views import MessageView
 
 urlpatterns = [
-    
+    path("<int:pk>/messages/", MessageView.as_view(), name="messages"),
+    path("<int:pk>/messages/", MessageView.as_view(), name="messages"),
 ]
 
 app_name = "chat"
--- beadeluxe/chat/views.py
+++ beadeluxe/chat/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.views.generic import View
 from django.views.decorators.http import require_POST
 from django.contrib.auth import get_user_model
@@ -8,8 +9,8 @@ from .models import Message
 
 # Create your views here.
 class MessageView(LoginRequiredMixin, View):
-    def get(self, request, pk):
-        course = Course.objects.get(pk=pk)
+    def get(self, request, course_id):
+        course = get_object_or_404(Course, id=course_id)
         membership = CourseUser.objects.filter(
             user=request.user,
             course=course
@@ -24,19 +25,21 @@ class MessageView(LoginRequiredMixin, View):
             "messages": messages
         })
 
-# class CreateMessageView(LoginRequiredMixin, View):
-#     def post(self, request, course_id):
-#         course = Course.objects.get(id=course_id)
-#         membership = CourseUser.objects.filter(
-#             user=request.user,
-#             course=course
-#         ).first()
+    def post(self, request, course_id):
+        course = get_object_or_404(Course, id=course_id)
+        membership = CourseUser.objects.filter(
+            user=request.user,
+            course=course
+        ).first()
 
-#         if not membership or membership.role not in ["student", "beadle"]:
-#             raise PermissionDenied
+        if not membership or membership.role not in ["student", "beadle"]:
+            raise PermissionDenied
         
-#         Message.objects.create(
-#             course=course
-#         )
+        Message.objects.create(
+            user=membership,
+            course=course,
+            content=request.POST.get("content"),
+            timestamp=request.POST.get("timestamp")
+        )
 
-#         return self.get(request, *args, **kwargs)
+        return self.get(request, course_id)
