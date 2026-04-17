65f44a603497cb0c2acf5af7896d9ef186e0d66d
Levi Si <anonymous.perrson09@gmail.com>
2026-04-17 13:48:02 +0800

    fixed url

--- beadeluxe/chat/urls.py
+++ beadeluxe/chat/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from chat.views import MessageView
 
 urlpatterns = [
-    path("<int:pk>/messages/", MessageView.as_view(), name="messages"),
-    path("<int:pk>/messages/", MessageView.as_view(), name="messages"),
+    path("<int:course_id>", MessageView.as_view(), name="messages"),
 ]
 
 app_name = "chat"
